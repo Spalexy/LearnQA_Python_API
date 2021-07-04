8a9fbe2fff26df7bb9d8e1d317e50a2ac25452f6
Alexander Pridacha <a.pridacha@bestdoctor.ru>
2021-07-05 00:22:20 +0300

    Headers test

--- test_lesson_3.py
+++ test_lesson_3.py
@@ -13,3 +13,11 @@ def test_request_for_cookies():
     print(dict(response.cookies))
     assert 'HomeWork' in response.cookies, 'There is no HomeWork field in cookies'
     assert response.cookies.get('HomeWork') == 'hw_value', 'Wrong value of HomeWork field'
+
+
+def test_request_for_headers():
+    response = requests.get('https://playground.learnqa.ru/api/homework_header')
+    print(dict(response.headers))
+    assert 'x-secret-homework-header' in response.headers, 'There is no x-secret-homework-header field in headers'
+    assert response.headers.get('x-secret-homework-header') == 'Some secret value', \
+        'Wrong value of x-secret-homework-header field'
