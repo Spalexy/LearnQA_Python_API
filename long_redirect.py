fc5293500f7edf0bea6229f2c22d4d99b9e1e330
Alexander Pridacha <a.pridacha@bestdoctor.ru>
2021-07-04 23:46:31 +0300

    Removing unnecessary scripts

--- long_redirect.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get('https://playground.learnqa.ru/api/long_redirect', allow_redirects=True)
-
-if response.history:
-    print("Всего произошло {} редиректа".format(len(response.history)))
-    print("Итоговый URL: {}".format(response.url))
-else:
-    print("Редиректов не было")
--- main.py
+++
@@ -1,7 +0,0 @@
-from requests import get
-
-response = get('https://playground.learnqa.ru/api/get_text')
-
-print(response)
-
-
