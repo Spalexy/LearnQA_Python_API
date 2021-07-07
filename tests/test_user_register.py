1a1fb0b639fc0fe020f402852a861363c10e0450
Alexander Pridacha <a.pridacha@bestdoctor.ru>
2021-07-07 19:28:47 +0300

    test create user with existing email

---
+++ tests/test_user_register.py
@@ -0,0 +1,9 @@
+import requests
+
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+
+
+class TestUserRegister(BaseCase):
+    def test_create_user_with_existing_email(self):
+        pass
