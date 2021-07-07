3516c85686205ca496d074bbb54032f162d1a07a
Spalexy <alex322p9@gmail.com>
2021-07-07 19:39:01 +0300

    add test data

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -6,4 +6,10 @@ from lib.assertions import Assertions
 
 class TestUserRegister(BaseCase):
     def test_create_user_with_existing_email(self):
-        pass
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': 'vinkotov@example.com'
+        }
